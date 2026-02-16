7fb368fafaeae1d2bba9082a94dd65e68a72c588
Phil Pluckthun <phil@kitten.sh>
2026-02-16 23:17:54 +0000

    Rebuild

--- packages/expo/build/winter/fetch/fetch.types.d.ts
+++ packages/expo/build/winter/fetch/fetch.types.d.ts
@@ -2,16 +2,27 @@
  * A fetch RequestInit compatible structure.
  */
 export interface FetchRequestInit {
-    body?: BodyInit;
+    body?: BodyInit | null;
     credentials?: RequestCredentials;
     headers?: HeadersInit;
     method?: string;
-    signal?: AbortSignal;
+    signal?: AbortSignal | null;
     redirect?: RequestRedirect;
     integrity?: string;
     keepalive?: boolean;
     mode?: RequestMode;
     referrer?: string;
     window?: any;
+}
+/**
+ * A fetch Request compatible structure.
+ */
+export interface FetchRequestLike {
+    url: string;
+    body: BodyInit;
+    credentials?: RequestCredentials;
+    method?: string;
+    signal?: AbortSignal;
+    redirect?: RequestRedirect;
 }
 //# sourceMappingURL=fetch.types.d.ts.map
